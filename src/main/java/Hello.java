249869c382ec57805cd404c9b5ce0d35e16e2d8a
MHMCAN <mmrcan@gmail.com>
2021-09-13 16:26:02 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello future");
+
+        System.out.println("Github is already published");
+        System.out.println("doing more work");
     }
 }
